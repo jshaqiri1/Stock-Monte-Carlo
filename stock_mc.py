655d145fa66274ee14b7da6e803f954cb0f5de5d
jshaqiri1 <127545831+jshaqiri1@users.noreply.github.com>
2023-03-11 22:08:18 -0500

    Add files via upload

---
+++ stock_mc.py
@@ -0,0 +1,72 @@
+import yfinance as yf
+import numpy as np
+import matplotlib.pyplot as plt
+import streamlit as st
+
+# Download stock data
+def download_data(ticker):
+    df = yf.download(ticker)
+    return df
+
+# Calculate returns
+def calculate_returns(df):
+    returns = np.log(1+df['Adj Close'].pct_change())
+    mu, sigma = returns.mean(), returns.std()
+    return mu, sigma
+
+# Simulate prices
+def simulate_prices(initial, mu, sigma):
+    sim_rets = np.random.normal(mu, sigma, 252)
+    sim_prices = initial * (sim_rets+1).cumprod()
+    return sim_prices
+
+# Plot prices
+def plot_prices(initial, sim_prices_list):
+    fig, ax = plt.subplots()
+    plt.axhline(initial, c='k')
+    for sim_prices in sim_prices_list:
+        plt.plot(sim_prices)
+    plt.title("Simulated Prices")
+    plt.xlabel("Trading Days")
+    plt.ylabel("Price ($)")
+    return fig
+
+# Main function
+def main():
+    # Page title
+    st.title("Stock Price Simulation")
+
+    # Sidebar
+    st.sidebar.title("Simulation Settings")
+
+
+    # Simulation settings
+    ticker = st.sidebar.text_input("Enter Ticker Symbol", "MSFT")
+    num_simulations = st.sidebar.slider("Number of Simulations", 1, 100, 10)
+
+    st.sidebar.write("""
+    ## Description
+    This tool simulates future stock prices through a Monte-Carlo analysis using historical price data to calculates the expected returns, standard deviation
+    and volatility. It then simulates future prices based on those parameters.
+    The tool can simulate multiple future price scenarios, allowing users to assess the potential
+    range of future prices and plan their investment strategy accordingly.
+    """)
+
+    
+    # Download data
+    df = download_data(ticker)
+    mu, sigma = calculate_returns(df)
+    initial = df['Adj Close'].iloc[-1]
+
+    # Run simulations
+    sim_prices_list = []
+    for i in range(num_simulations):
+        sim_prices = simulate_prices(initial, mu, sigma)
+        sim_prices_list.append(sim_prices)
+    
+    # Plot all simulations
+    fig = plot_prices(initial, sim_prices_list)
+    st.pyplot(fig)
+
+if __name__ == '__main__':
+    main()
